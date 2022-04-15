4852a6a777b523865f7405d0a7c4f10ae7a05ba3
Allison Tong <allisontong04@gmail.com>
2022-04-15 12:36:11 -0700

    help
    please

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,3 @@
-
 function readClicked(){
           $.ajax({
             url: "/read",
@@ -8,17 +7,23 @@ function readClicked(){
                 if (data.error)
                   alert("bad");
                 else {
-                  let pa = document.createElement("pa");
-                  let p = document.getElementById("parag");
-                  for(let i=0;i<data.val.length+1;i++)
+                    let pa = document.createElement("pa");
+                    let p = document.getElementById("parag");
+                  if(p)
                   {
-                      pa.innerHTML = "<br /> Name:" + data.val[i].name + "<br />"+  "Ingredients:" + data.val[i].ingredients;
+                  for(let i=0;i<data.val.length;i++)
+                  {
+                    console.log(data.val[i].name);
+                      pa.innerHTML += "<br /> Name:" + data.val[i].name + "<br />"+  "Ingredients:" + data.val[i].ingredients;
                       pa.innerHTML += "<br /> Instructions:" + data.val[i].instructions;
                       pa.innerHTML += "<br /> Allergies:" + data.val[i].allergies;
                       pa.innerHTML += "<br /> Diets:" + data.val[i].diet;
-
+                  }
                   p.appendChild(pa);
                   }
+                  else
+                    console.log(p)
+
                 }
               } ,
             dataType: "json"
