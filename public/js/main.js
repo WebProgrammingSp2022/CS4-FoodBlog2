5834dc75ee27b88bec5f645889f32f8eefebdfff
TaliaG16 <taliagoody@gmail.com>
2022-04-21 10:29:06 -0700

    text is not as bad
    end pls

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,5 @@
 //lists
+
 function readClicked(){
           $.ajax({
             url: "/read",
@@ -10,16 +11,16 @@ function readClicked(){
                 else {
                     let pa = document.createElement("pa");
                     let p = document.getElementById("parag");
-                  for(let i=0;i<data.val.length;i++)
-                  { 
-                      p.innerHTML+="<br/>"
-                      p.innerHTML += "<br /> Name:" + data.val[i].name + "<br />"+  "Ingredients:" + data.val[i].ingredients;
-                      p.innerHTML += "<br /> Instructions:" + data.val[i].instructions;
-                      p.innerHTML += "<br /> Allergies:" + data.val[i].allergies;
-                      p.innerHTML += "<br /> Diets:" + data.val[i].diet;
-                  }
-                  p.appendChild(pa);
 
+                    for(let i=0;i<data.val.length;i++)
+                    {
+                          p.innerHTML+="<br/>"
+                          p.innerHTML += "<br /> Name:" + data.val[i].name + "<br />"+  "Ingredients:" + data.val[i].ingredients;
+                          p.innerHTML += "<br /> Instructions:" + data.val[i].instructions;
+                          p.innerHTML += "<br /> Allergies:" + data.val[i].allergies;
+                          p.innerHTML += "<br /> Diets:" + data.val[i].diet;
+                    }
+                    p.appendChild(pa);
                 }
               } ,
             dataType: "json"
