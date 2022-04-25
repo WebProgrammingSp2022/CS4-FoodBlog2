5c50e735f65f81cec02c4fff78c5e3f7d4913062
Allison Tong <103070858+ATong1567@users.noreply.github.com>
2022-04-24 21:00:30 -0700

    favicon stuff :)

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 var routes = require("./routes");
 
 var app = express();
+app.use('/favicon.ico', express.static('favicon.png'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use('/js', express.static('./public/js'));
