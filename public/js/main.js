bb12b35cf5fa80821ce5e848b8ad9f4049ae5cbd
Allison Tong <103070858+ATong1567@users.noreply.github.com>
2022-04-24 22:18:44 -0700

    I did something?
    It just prints out a whole bunch of images. its meh

--- public/js/main.js
+++ public/js/main.js
@@ -6,12 +6,19 @@ function readClicked(){
             type: "GET",
             data: {},
             success: function(data){
+                  //  let ar = [];
                     let pa = document.createElement("pa");
                     let p = document.getElementById("parag");
+                  //  imgloop();
                     for(let i=0;i<data.val.length;i++)
                     {
+                        //  ar.push("/images/" + data.val[i].filename2)
+                          var img = new Image(200, 200);
+                          img.src = "/images/" + data.val[i].filename2;
 
-                          image.src = "/images/"+ data.val[i].filename2;
+                          var src = document.getElementById("images");
+                          src.appendChild(img);
+                          //image.src = "/images/"+ data.val[i].filename2;
                           p.innerHTML += "<br/>"
                           p.innerHTML += "<br /> Name:" + data.val[i].name + "<br />"+  "Ingredients:" + data.val[i].ingredients;
                           p.innerHTML += "<br /> Instructions:" + data.val[i].instructions;
@@ -24,7 +31,23 @@ function readClicked(){
           });
   return false;
 }
+function imgloop()
+  {
+    var ar = [];
+    ar.push("http://www.google.com/intl/en_com/images/logo_plain.png");
+    ar.push("https://static.pexels.com/photos/28773/pexels-photo-28773.jpg");
+    ar.push("https://static.pexels.com/photos/1984/black-and-white-city-man-people.jpg");
 
+    for (i = 0; ar.length > i; i++)
+    {
+      var img = new Image(200, 200);
+      img.src = ar[i];
+
+      var src = document.getElementById("images");
+      src.appendChild(img);
+    }
+
+}
 $(document).ready(function(){
  readClicked();
 });
